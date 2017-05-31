f761cd9927022be6a7003c0b11de1e837e205c39
stan <3421902@qq.com>
2017-05-31 11:03:32 +0800

    aaaa

--- config/index.js
+++ config/index.js
@@ -9,6 +9,7 @@ module.exports = {
         index: path.resolve(__dirname, '../dist/index.html'),
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
+        //assetsPublicPath: '',
         assetsPublicPath: 'http://45.32.23.77:8081/',
         productionSourceMap: true,
         // Gzip off by default as many popular static hosts such as
