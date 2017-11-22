c80f99c8dcc06550ea3015735735e47dfd79f02a
“stanhu” <3421902@qq.com>
2017-11-22 17:31:52 +0800

    remove something

--- server/controllers/cityController.js
+++ server/controllers/cityController.js
@@ -1,9 +1,5 @@
-const APIError = require('../rest').APIError;
-const Link = require('../model/link')
 const Result = require('../model/result.js')
-const Tool = require('../tool/tool')
 const City = require('../model/city')
-const DB = require('../sqlhelp/mysql')
 module.exports = {
     'GET /api/cities': async (ctx, next) => {
        let result = await City.getCities()
