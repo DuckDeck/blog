ef174282905434ff5470cfbbd47f562800b2a601
“stanhu” <3421902@qq.com>
2018-09-03 11:22:19 +0800

    process code

--- server/sqlhelp/mysql.js
+++ server/sqlhelp/mysql.js
@@ -16,13 +16,6 @@ var poolCity  = mysql.createPool({
     password        : '123456',  
     database        : 'city' ,
   });
-  var poolTemp  = mysql.createPool({  
-    connectionLimit : 10,  
-    host            : 'localhost',  
-    user            : 'test',  
-    password        : '123456',  
-    database        : 'temp' ,
-  });
 //Issue Point there is a bug in the mysql pool system, it looks like it can cache some query when the sql is the some and the 
 //request result will not change, this is not looks
 //it's not a bug ,it's my issue
@@ -81,31 +74,6 @@ db.exec = function(sql,data,db_name='blog'){
                   resolve(res) 
               }); 
         }
-        else if (db_name == 'temp'){
-            poolTemp.query(sql,data, function(err, rows, fields) {  
-                if (err) {
-                    console.log(err)
-                    pool.query('insert into blog_error values(0,?,?,?,?,?,?)',
-                    [err.errno,err.name,err.code,err.message,err.toString(),new Date().getTime()],(err,rows,fields)=>{
-                    })
-                    reject(result.create(-50))
-                    return;    
-                  }
-                  let res = result.create(0,rows)
-                  if(sql.trim().substr(0,6) == 'insert' || sql.trim().substr(0,6) == 'replac'){
-                      if(rows.insertId){
-                          res.data = {id:rows.insertId}
-                      }
-                  }
-                  if(sql.trim().substr(0,6) == 'delete'){          
-                         res.data = {}
-                  }
-                  console.log('exec completed')
-                  resolve(res) 
-              }); 
-
-        }
-        
     }) 
 }
 module.exports = db; 
