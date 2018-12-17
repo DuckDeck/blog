5fbd02b7a50f7a37d633d73c3ca4015070fb4b53
“stanhu” <3421902@qq.com>
2018-12-17 12:13:46 +0800

    process code

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -15,13 +15,10 @@ module.exports = {
         let id = ctx.params.userId
         let c = {}
         let res = await DB.exec('select count(id) as count from message_comment where receive_id = ? and read_status = 0',[id])
-        console.log(res)
         c.comment_unread_count = res.data[0].count
         res = await DB.exec('select count(id) as count from message_like where receive_id = ? and read_status = 0',[id])
-        console.log(res)
         c.like_unread_count = res.data[0].count
         res = await DB.exec('select count(id) as count from message_attention where receive_id = ? and read_status = 0',[id])
-        console.log(res)
         c.attention_unread_count = res.data[0].count
         // res = await DB.exec('select count(id) as count from chat_message where receive_id = ? and read_status = 0',[id])
         // console.log(res)
@@ -42,20 +39,64 @@ module.exports = {
             return
         }
         result = Check.checkPage(ctx)
-        if(result.code != 0){
+        if(result){
             ctx.rest(result)
             return
         }
         result = Check.checkNum(ctx.params,'type')
-        if(result.code != 0){
+        if(result){
             ctx.rest(result)
             return
         }
         let index = parseInt(ctx.params.index)
         let size = parseInt(ctx.params.size)
         let user_id = ctx.params.userId
-        let type = ctx.params.type
+        let type = parseInt(ctx.params.type)
+        let sql = ''
+        switch (type) {
+            case 1:  //评论信息
+                sql = 'select * from message_comment where receive_id = ? order by message_time desc limit ?,?'
+                result = await DB.exec(sql,[user_id,index,size])
+                console.log(result)
+                break;
+            case 2: //用户喜欢信息
+                sql = 'select * from message_like where receive_id = ? order by message_time desc limit ?,?'
+                result = await DB.exec(sql,[user_id,index,size])
+                break;
+            case 3: //用户喜欢信息
+                sql = 'select * from message_attention where receive_id = ? order by message_time desc limit ?,?'
+                result = await DB.exec(sql,[user_id,index,size])
+
+                break;
+            case 4:
+                sql = 'SELECT sender_id,count(id) as count FROM chat_message where receive_id = ? group by  sender_id limit ?,?'
+                result = await DB.exec(sql,[user_id,index,size])
+            default:
+                break;
+        }
         
+        if(type != 1){
+            let ids = result.data.map(s=>{
+                return s.sender_id
+            })
+            if(ids.length > 0){
+                let tmp = new Set(ids)
+                ids = [...tmp].join(',')
+                sql = `select user_id,user_real_name,user_image_url from user_info where user_id in (` + ids + `)`
+                let res = await DB.exec(sql)
+                result.data = result.data.map(s=>{
+                    let info = res.data.find(k=>{
+                        return k.user_id == s.sender_id
+                    })
+                    if(info != null){
+                        s.user_info = info
+                    }
+                    return s
+                })
+            }
+        }
+        console.log(result)
+        //这里要设计一下已读信息
         ctx.rest(result)
     },
 
--- src/page/blog/userInfo/myMessage.vue
+++ src/page/blog/userInfo/myMessage.vue
@@ -1,11 +1,21 @@
 <template>
       <div  class="container" >
           <div class="main-page">
-            <div class="myAttentions"> 
-               
-                
+            <div class="message_item" > 
+               <div> 
+                   <i class="fa fa-commenting"></i> 评论
+               </div>
+                <div> 
+                   <i class="fa fa-heart"></i> 喜欢和赞
+               </div>
+               <div> 
+                   <i class="fa fa-user-plus"></i> 关注
+               </div>
+               <div> 
+                   <i class="fa fa-wechat"></i> 私信
+               </div>
             </div>
-            <div class="myAttentionsArticles" v-loading="loading">
+            <div class="message_content" v-loading="loading">
             
             </div>
           </div>
@@ -24,11 +34,10 @@ import loadMore from './../com/loadMore.vue'
         data(){
             return {
                 userId:0,
-              
                 isLoadinMore:false,
-                attentionedUserArticlesCount:0,
-                type:0,
+                type:2,
                 loading:false,
+                itemCount:0
             }
         },
         async mounted(){
@@ -59,21 +68,30 @@ import loadMore from './../com/loadMore.vue'
     min-height: 800px;
     display: flex;
 }
-.myAttentions{
+.message_item{
     min-width: 200px;
     border-right: 2px solid peru
 }
-.attentionedUsers{
+.message_content{
     display: flex;
     padding: 10px 20px 10px 20px;
     margin-bottom: 2px;
 }
-.attentionedUsersSelected{
-    display: flex;
-    padding: 10px 20px 10px 20px;
-    background-color: #dddddddd;
-    margin-bottom: 2px;
+.message_item div{
+
+    padding: 10px 10px 10px 20px;
+    cursor: pointer;
 }
+.message_item div:hover{
+    background: #dddddddd
+}
+
+.message_item div i{
+    color: orangered;
+    margin-right: 5px;
+    font-size: 23px;
+}
+
 .attentionedUsers:hover{
     cursor: pointer;
     background: #dddddddd;
