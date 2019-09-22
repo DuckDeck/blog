d052a64cd45854d2d2d6c3006e7d9e74ddf6be06
Stan Hu <3421902@qq.com>
2019-09-22 10:30:13 +0800

    fix the tuyimm

--- crawler/tuyimm.js
+++ crawler/tuyimm.js
@@ -3,24 +3,31 @@
 
 var cheerio = require('cheerio')
 const axios = require('axios')
-var url = "http://www.tuyimm.net/thread-12773-1-1.html"
+const os = require('os')
+var url = "http://www.tuyim.com/thread-22247-1-1.html"
 var fs = require('fs')
 var request = require("request")
 var imagePages = []
 url =  encodeURI(url)
+
+
+
 // fs.mkdirSync(__dirname + '/' + 'crawler_download' + '/' + 123)
 function load(){
+
+
     axios.get(url).then(res=>{
         var $ = cheerio.load(res.data)
+        let title = $('span#thread_subject')['0'].children[0].data
         let s  = Array.from($('img.lazy')).map(k=>{
-            return k.attribs['data-original']
+            return k.attribs['zoomfile']
         })
         //console.log(s)
         // imagePages = s.map(k=>{
         //     return   k.attribs.href
         // })
         for(let u of s){
-             downloadImg('123',u,url)
+             downloadImg(title,u,url)
         }
         
     }).catch(err=>{
@@ -31,8 +38,9 @@ load()
  
 function downloadImg(name,url,origin_url){
     console.log(url)
-    if(!fs.existsSync(__dirname + '/' + 'crawler_download' + '/' + name)){
-        fs.mkdirSync(__dirname + '/' + 'crawler_download' + '/' + name)
+    
+    if(!fs.existsSync(__dirname + '\\' + 'crawler_download' + '\\' + name)){
+        fs.mkdirSync(__dirname + '\\' + 'crawler_download' + '\\' + name)
     }
     if(!fs.existsSync(__dirname + '/' + 'crawler_download' + '/' + name + '/' + 'tuyyimm.txt')){
         fs.writeFileSync(__dirname + '/' + 'crawler_download' + '/' + name + '/' + 'tuyyimm.txt',origin_url)
